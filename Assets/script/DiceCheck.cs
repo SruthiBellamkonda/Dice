c19440dcb4a18b920d5ebbde0cf7beb1ec7ed88f
Sruthi Bellamkonda <sBellamkonda@rythmos.com>
2020-09-29 17:16:34 +0530

    DiceNumberCorrection

--- Assets/script/DiceCheck.cs
+++ Assets/script/DiceCheck.cs
@@ -20,22 +20,22 @@ public class DiceCheck : MonoBehaviour
 			switch (col.gameObject.name)
 			{
 				case "Side1":
-					DiceNumber.diceNumber = 1;
+					DiceNumber.diceNumber = 6;
 					break;
 				case "Side2":
-					DiceNumber.diceNumber = 2;
-					break;
-				case "Side3":
-					DiceNumber.diceNumber = 3;
-					break;
-				case "Side4":
-					DiceNumber.diceNumber = 4;
-					break;
-				case "Side5":
 					DiceNumber.diceNumber = 5;
 					break;
+				case "Side3":
+					DiceNumber.diceNumber = 4;
+					break;
+				case "Side4":
+					DiceNumber.diceNumber = 3;
+					break;
+				case "Side5":
+					DiceNumber.diceNumber = 2;
+					break;
 				case "Side6":
-					DiceNumber.diceNumber = 6;
+					DiceNumber.diceNumber = 1;
 					break;
 			}
 		}
